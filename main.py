b26c2ad512821bdb57ff04d37729a398f6aee13e
AliAnbrloo <anbarloo@protonmail.com>
2023-09-07 13:11:08 +0330

    افزودن exit  , list  (فایل دیتابیس هنوز ساخته می شود اما پر نمی شود)

--- main.py
+++ main.py
@@ -12,33 +12,38 @@ class sp(): # slow print
 CLS()
 
 Now = str(datetime.datetime.now())
-dbjson = {}
+#dbjson = {}
 
 class Database():
-    def start():
+    def __init__(self):
+        self.dbjson = {}  # تعریف متغیر dbjson به عنوان یک متغیر عضو
+
+    def start(self):
         try:
-            with open("XNPDB.json" , 'r') as DbFile:
-                DB = json.load(DbFile)
-                dbjson = DB
-        except:
-            with open("XNPDB.json" , 'w') as DbFile:
-                json.dump(dbjson, DbFile,indent=4)
+            with open("XNPDB.json", 'r') as DbFile:
+                pass
+                #DB = json.load(DbFile)
+                #self.dbjson = DB  # به dbjson اطلاعات از فایل می‌خوانیم
+        except FileNotFoundError:
+            # در صورتی که فایل وجود نداشته باشد، متغیر dbjson خالی می‌ماند
+            pass
     def idkdb(self):
         with open("XNPDB.json" , 'r') as DbFile:
             DB = json.load(DbFile)
             print(DB)
     def newinfo(self):
-        with open("XNPDB.json" , 'w') as DbFile:
-            json.dump(dbjson, DbFile, indent=4)
+        with open("XNPDB.json", 'w') as DbFile:
+            json.dump(self.dbjson, DbFile, indent=4)  # ذخیره dbjson در فایل
 
-
+db = Database()  # ایجاد یک نمونه از کلاس Database
+db.start()  # شروع دیتابیس
         
 
 class delete():
     def __init__(self , Path , sure):
         if sure == True:
             try:
-                del dbjson[Path]
+                del db.dbjson[Path]
                 sp("Fille remove successfully! \n")
             except:
                 sp("File not found!\n")
@@ -49,7 +54,7 @@ class search():
         if idt == True:
             CLS()
             try:
-                print(dbjson[id])
+                print(db.dbjson[id])
                 global editthis
                 editthis = input("\n \nWant to edit this? y/n").upper()
                 if editthis == "YES" or editthis =="Y":
@@ -61,29 +66,29 @@ class search():
         if idt == False:
             CLS()
             try:
-                print(dbjson[FileName])
+                print(db.dbjson[FileName])
             except:
                 sp("File not found!\n")
 class create():
     def __init__ (self , FileName , Content , id , data = Now):
-        dbjson[FileName] = [Content , id , data]
+        db.dbjson[FileName] = [Content , id , data]
         Database().newinfo
         sp("File created successfully! \n")
-        print(dbjson)
+        print(db.dbjson)
 
 class Add():
     def __init__ (self,FileName):
-        mn = dbjson[FileName]
+        mn = db.dbjson[FileName]
         print(f"Current content : {mn[1]}")
         ec = input("What should i add? : ")
         cd = mn[1]
         cd += ' ' + ec
         mn[1] = cd
         sp("Done! \n")
-Database.start()
+#Database.start()
 sp("Hello dear! for help , enter Help! \n")
 while (True):
-    MainInput = input("-> Enter S/C/D/A/ : ").upper()
+    MainInput = input("-> Enter S/C/D/E/L/A/ : ").upper()
     #=("-> e/s/c/d/h : ").upper()
     if MainInput =="C" or MainInput =="CREATE":
         cfnme = input("Enter the file name : ")
@@ -100,12 +105,18 @@ while (True):
             search(False,000,fnme)
     elif MainInput=="D" or MainInput=="DELETE" or MainInput=="REMOVE" or MainInput=="R":
         patf = input("Enter file name : ")
-        sure_ = input("Are your sure? True/False")
+        sure_ = input("Are your sure? True/False : ")
         if sure_ =="True":
             sure_ = True
             delete(patf,sure_)
     elif MainInput=="HELP" or MainInput=="H":
         CLS()
-        sp("Hello dear! \n Enter S for search in the files\n Enter D for  delete the files \n Enter A for edit files \n Enter C for create file")
+        sp("Hello dear! \n Enter S for search in the files\n Enter D for  delete the files \n Enter A for edit files \n Enter C for create file \n Enter E for exit \n Enter L for view list of files \n")
+    elif MainInput=="EXIT" or MainInput=="E":
+        exit()
+    elif MainInput=="L" or MainInput=="LIST":
+        print(db.dbjson)
+    elif MainInput=="CLS" or MainInput=="CLEAR":
+        CLS()
     else:
         print("Not now!")
